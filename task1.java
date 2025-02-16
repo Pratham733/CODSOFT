37e245fc50d8c3171c07c9051310c452d32cd805
Pratham Singh <119106718+Pratham733@users.noreply.github.com>
2025-02-16 20:26:45 +0530

    Update task1.java

--- task1.java
+++ task1.java
@@ -2,7 +2,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {  // Changed from NumberGame to Main
+public class Main {  
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
